fb5ef03e7b0257d602565a4fe197682741303a7e
Bhagjot <singhbhagjot@gmail.com>
2023-05-09 19:15:25 +1200

    Update version_1.py
    My DIP teacher gave me feedback and this is a fix to all the problems she told me. The program would continue running its process even after the function called stopped. This was fixed with breaks every time a function is called in a loop. There was a usability error where unnecessary questions were being asked. Fixed by removing them and using functions. The program would ask you to exit many times before exiting, fixed by using a break instead of calling the previous function every time. There are now more comments specifying what is happening correctly. All other minor issues were fixed.

--- version_1.py
+++ version_1.py
@@ -2,98 +2,103 @@
 # This is the first iteration of the Cafe Menu application.
 # B.Singh 09/04/2023
 
-# json is imported to use an external file to store account details
+# json library is imported
 import json
 
-# account.json is imported as a dictionary
+# account.json file is opened which stores the login information
 accounts = json.load(open("accounts.json", "r"))
 
-# Variables which hold the quantity of items
+# These variables hold the quantity of each item available for sale
 nachos = 0
 quiches = 0
 chicken_burgers = 0
 sushi = 0
 
-# Variables which hold the price of the items
+# These variables hold the price of each item available for sale
 nachos_price = 4.8
 quiches_price = 4.8
 chicken_burgers_price = 4.8
 sushi_price = 5.8
 
-# Variables which hold the total prices
+# These variables hold the total prices of each item available for sale
 nachos_total_price = 0
 quiches_total_price = 0
 chicken_burgers_total_price = 0
 sushi_total_price = 0
 total_price = 0
 
-# This is the main routine
+# This function is the main routine which the user interacts with first
 def main_routine():
     while True:
-        print("\nWelcome to the Cafe Menu Application!\n\n1. Login\n2. Create an account\n3. Exit")
+        # These are options listed for the user to choose
+        print("\n1. Login\n2. Create an account\n3. Exit")
         option = input("\nPlease select and option above: ")
         
-        # This is when the user wants to log in
+        # This is when the user wants to log in to their account
         if option == "1":
             login()
+            break
 
         # This is when the user wants to create an account
         elif option == "2":
             create_account()
+            break
 
         # This is when the user wants to exit the program
         elif option == "3":
-            quit()
+            print("\nThanks for using the Cafe Menu Application!")
+            break
 
         # This is when the user inputs and invalid input
         else:
             print("Try again!")
 
-# This is the function which works when the user wants to login
+# This function is called if the user wants to log in to their account
 def login():
     while True:
+        # This is where the username is asked
         username = input("\nUsername: ")
 
-        # This is where the username is checked 
+        # This is where the username is in the file
         if username in accounts:
+            # This is where the password is asked
             password = input("Password: ")
 
-            #This is where the user's password is checked
+            #This is where the password is correct
             if password == accounts.get(username, None):
                 menu()
+                break
+            
             # This is where if the password is incorrect
             else:
-                print("\nInvalid password. Try again!")
-                login()
-
-        # This is where the username does not exist
-        else:
-            try_again = input(f"\nThere is no account with {username} as a username. Do you want to try again? (y/n) ")
-            if try_again == "y":
-                pass
-            else:
+                print("\nWhat would you like to do?")
                 main_routine()
+                break
 
-# This is when the user wants to create an account
+        # This is where the username is not in the file
+        else:
+            print(f"\nThere is no account created with the username: {username}\nWhat would you like to do?")
+            main_routine()
+            break
+
+# This functio nis called if the user wants to create an account
 def create_account():
     global accounts
 
     while True:
         try:
-            # This is where the user's age is being checked
+            # This is where the user's age is asked
             user_age = int(input("What is your age? "))
 
-            # This is when the user's age is valid
+            # This is where the age is checked if it meets the requirements
             if user_age >= 13 and user_age <=18:
                 username = input("\nPlease choose an username: ")
 
                 # This is where the username is taken
                 if username in accounts:
-                    try_again = input(f"\nThe username {username} is taken. Do you want to try again? (y/n) ")
-                    if try_again == "y":
-                        pass   
-                    else:
-                        main_routine()
+                    print(f"\nThe username {username} is taken.\nWhat would you like to do?")
+                    main_routine()
+                    break
 
                 # This is where the username is not taken
                 else:
@@ -102,34 +107,105 @@ def create_account():
                     json.dump(accounts, open("accounts.json", "w"))
                     print("\nAccount created successfully.")
                     main_routine()
-            # This is where the user does not meet the prerequisites 
-            else:
-                print("You can not create an account.")
-                main_routine()
-        # This is where the user's age is unvalid
-        except ValueError or SyntaxError or IndexError:
-            print("\nTry again")
+                    break
 
-# This is where the user can select if they want to order or view the total
+            # This is where the user's age does not meet the requirements 
+            else:
+                print("Age must be between 13 to 18 to create an account.\nThanks for using the Cafe Menu Application!")
+                break
+
+        # This is where the user's age is invalid
+        except ValueError:
+            print("\nThat is not a valid age. What would you like to do?")
+            main_routine()
+            break
+
+# This function is called when the user has logged in to their account
 def menu():
     while True:
-        option_chosen = input("\n1. View basket\n2. Add to basket\n3. Exit\nPlease select an option above: ")
+        option_chosen = input("\nYou are now logged in, what would you like to do?\n1. View Menu and add items to basket\n2. View Basket\n3. Exit\nPlease select an option above: ")
 
         # This is if the user wants to view the total
         if option_chosen == "1":
-            view_basket()
+            add_to_basket()
+            break
 
         # This is if the user wants to order
         elif option_chosen == "2":
-            add_to_basket()
+            view_basket()
+            break
 
         # This is if the user wants to exit
         elif option_chosen == "3":
-            main_routine()
+            print("\nThanks for using the Cafe Menu Application!")
+            break
 
         # This is if the user inputs something invalid
         else:
-            print("\nTry again!")
+            print("\nPlease input numbers: 1, 2, or 3")
+
+# This function is called if the user wants to view the menu and add items to basket
+def add_to_basket():
+    # All variables are called to use in this function
+    global nachos
+    global nachos_price
+    global nachos_total_price
+    global quiches
+    global quiches_price
+    global quiches_total_price
+    global chicken_burgers
+    global chicken_burgers_price
+    global chicken_burgers_total_price
+    global sushi
+    global sushi_price
+    global sushi_total_price
+
+    # This is where the items avaible to sale and other options are displayed
+    while True:
+        try:
+            print("\nItems for sale:\n1. Nachos (v) ${:.2f}\n2. Quiches - varied (v) ${:.2f}\n3. Chicken Burger ${:.2f}\n4. Sushi Box (g) ${:.2f}\n5. View Basket\n6. Exit".format(nachos_price, quiches_price, chicken_burgers_price, sushi_price))
+            order = int(input("\nPlease enter a number from above to add to basket: "))
+
+            # This is when the user wants to order Nachos
+            if order == 1:
+                quantity = int(input("\nHow many Nacho boxes would you like to order? "))
+                nachos += quantity
+                nachos_total_price += nachos * nachos_price
+
+            # This is when the user wants to order Quiches
+            elif order == 2:
+                quantity = int(input("\nHow many Quiches would you like to order? "))
+                quiches += quantity
+                quiches_total_price += quiches * quiches_price
+                    
+                # This is when the user wants to order Chicken Burgers
+            elif order == 3:
+                quantity = int(input("\nHow many Chicken Burgers would you like to order? "))
+                chicken_burgers += quantity
+                chicken_burgers_total_price += chicken_burgers * chicken_burgers_price
+
+                # This is when the user wants to order Sushi
+            elif order == 4:
+                quantity = int(input("\nHow many Sushi boxes would you like to order? "))
+                sushi += quantity
+                sushi_total_price += sushi * sushi_price
+
+            # This is when the user wants to view the basket
+            elif order == 5:
+                view_basket()
+                break
+            
+            # This is when the user wants to exit the program
+            elif order == 6:
+                print("\nThanks for using the Cafe Menu Application!")
+                break
+
+            else:
+                print("\nPlease enter 1, 2, 3, 4, 5, or 6.\n")
+                
+        # This is if the user inputs something invalid
+        except ValueError:
+            print("\nPlease enter 1, 2, 3, 4, 5, or 6.\n")
 
 # This is where the user can view the total
 def view_basket():
@@ -162,64 +238,19 @@ def view_basket():
         print("\nThe total price is ${:.2f}".format(total_price))
 
         # This is so the user can leave
-        exit_view_basket = input("\nDo you want to leave? (y/n) ")
-        if exit_view_basket == "y":
-            menu()
+        exit_view_basket = input("\nWhat would you like to do?\n1. View Menu\n2. Exit\nPlease select an option above: ")
+        if exit_view_basket == "1":
+            add_to_basket()
+            break
 
-# This is where the user can order items
-def add_to_basket():
-    # These are all the vriables retrieved which need to be updated when something is added to the basket
-    global nachos
-    global nachos_price
-    global nachos_total_price
-    global quiches
-    global quiches_price
-    global quiches_total_price
-    global chicken_burgers
-    global chicken_burgers_price
-    global chicken_burgers_total_price
-    global sushi
-    global sushi_price
-    global sushi_total_price
+        elif exit_view_basket == "2":
+            print("\nThanks for using the Cafe Menu Application!")
+            break
 
-    # This is where the menu is displayed
-    try:
-        while True:
-            print("\nItems for sale:\n1. Nachos (v) ${:.2f}\n2. Quiches - varied (v) ${:.2f}\n3. Chicken Burger ${:.2f}\n4. Sushi Box (g) ${:.2f}\n5. Exit".format(nachos_price, quiches_price, chicken_burgers_price, sushi_price))
-            order = int(input("\nPlease enter a number from above: "))
-
-            # This is if the user wants to order Nacho Boxes
-            if order == 1:
-                quantity = int(input("\nHow many Nacho boxes would you like to order? "))
-                nachos += quantity
-                nachos_total_price += nachos * nachos_price
-
-            # This is if the user wants to order Quiches
-            elif order == 2:
-                quantity = int(input("\nHow many Quiches would you like to order? "))
-                quiches += quantity
-                quiches_total_price += quiches * quiches_price
-                
-            # This is if the user wants to order Chicken Burgers
-            elif order == 3:
-                quantity = int(input("\nHow many Chicken Burgers would you like to order? "))
-                chicken_burgers += quantity
-                chicken_burgers_total_price += chicken_burgers * chicken_burgers_price
-
-            # This is if the user wants to order Sushi Boxes
-            elif order == 4:
-                quantity = int(input("\nHow many Sushi boxes would you like to order? "))
-                sushi += quantity
-                sushi_total_price += sushi * sushi_price
-
-            # This is if the user wants to exit
-            elif order == 5:
-                menu()
-            
-    # This is if the user inputs something invalid
-    except ValueError or SyntaxError or IndexError:
-        print("\nTry again!")
+        else:
+            print("\nPlease type 1 or 2.\n")
 
 # This is so the program can not be run through another program
 if __name__ == "__main__":
+    print("Welcome to the Cafe Menu Application!")
     main_routine()
